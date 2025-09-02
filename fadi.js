997e9861c370a0618c7a4c02dbe4bb7219e86d50
fadil <f6289217@gmail.com>
2025-09-02 20:36:12 +0600

    text

--- fadi.js
+++
@@ -1,4 +0,0 @@
-
-let a1 = document.querySelector(".a1")
-
-a1.innerHTML = "hello fadil"
--- index.js
+++ index.js
@@ -2,8 +2,3 @@ let result = document.querySelector(".result")
 
 result.innerHTML = "hello world"
 
-
-
-let a1 = document.querySelector(".a1")
-
-a1.innerHTML = "hello fadil"
